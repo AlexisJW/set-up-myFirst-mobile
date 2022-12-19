4bd1dff2422d8a18edcad4b96777b23ebeccc702
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2022-12-19 14:54:33 -0500

    Working in the Js file

---
+++ index.js
@@ -0,0 +1,18 @@
+const hamburgerButton = document.querySelector('.menu-phone');
+const nav = document.querySelector('.nav-wrapper');
+const closeBtn = document.querySelector('.close-menu');
+const ancreLinks = nav.getElementsByTagName('a');
+
+hamburgerButton.addEventListener('click', () => {
+  nav.classList.add('open');
+});
+
+closeBtn.addEventListener('click', () => {
+  nav.classList.remove('open');
+});
+
+for(let i = 0; i < ancreLinks.length; i++) {
+  ancreLinks[i].addEventListener('click', () => {
+    nav.classList.remove('open');
+  });
+}
